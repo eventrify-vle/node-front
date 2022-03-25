5e644a4ed853c80a5929b181c84cebdb2b750afc
Pavle Andric <pavle@eventrify.eu>
2022-03-25 17:20:11 +0100

    use http insted grpc

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,15 @@ const daprHost = "127.0.0.1"
 const daprPort = "3500"
 
 async function startDapr() {
-  const client = new DaprClient(daprHost, daprPort, CommunicationProtocolEnum.GRPC)
-  app.get("/:grpc", async (req, res) => {
+  const client = new DaprClient(daprHost, daprPort, CommunicationProtocolEnum.HTTP)
+  app.get("/:invoke", async (req, res) => {
     try {
       const result = await client.invoker.invoke("node-back", "status", HttpMethod.GET)
-      console.log(JSON.stringify(result, null, 2))
+      res.status(200).json(result)
     } catch (error) {
       console.log(error)
       res.status(400).json({ message: "status invoker failed" })
     }
-    res.status(200).json({ message: "status invoked successfully" })
   })
 }
 
@@ -28,3 +27,19 @@ startDapr().catch((e) => {
   console.error(e)
   process.exit(1)
 })
+
+// az containerapp create \
+//   --name app-node-front \
+//   --resource-group $RESOURCE_GROUP \
+//   --environment $CONTAINERAPPS_ENVIRONMENT \
+//   --image emplregistry.azurecr.io/node-front \
+//   --target-port 4001 \
+//   --ingress 'external' \
+//   --min-replicas 1 \
+//   --max-replicas 1 \
+//   --enable-dapr \
+//   --dapr-app-port 3500 \
+//   --dapr-app-id node-front \
+//   --registry-login-server $REGISTRY_LOGIN_SERVER \
+//   --registry-username $REGISTRY_USERNAME \
+//   --registry-password $REGISTRY_PASSWORD
